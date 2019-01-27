b544732face112e81a377e9248ddac3c9fae93f5
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2019-01-27 20:23:00 +0000

    Updated git and vim configurations.

--- home-manager.nix
+++ home-manager.nix
@@ -1,13 +1,20 @@
 { pkgs }:
 
 {
+
+  programs.vim = {
+    enable = true;
+    plugins = [ "vim-airline" ];
+  };
+
   programs.git = {
     enable = true;
     userName = "Dylan Tisdall";
     userEmail = "mtisdall@pennmedicine.upenn.edu";
+    extraConfig.core.editor = "vim";
+    extraConfig.diff.tool = "vimdiff";
+    extraConfig.difftool.prompt = "false";
   };
 
-  programs.vim = {
-    enable = true;
-  };
+  programs.home-manager.enable = true;
 }
