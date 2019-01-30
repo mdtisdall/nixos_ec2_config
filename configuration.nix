9118e795a199814b60e5a3b33429fc2bb0edfe1d
System administrator <root@ip-172-31-23-156.ec2.internal>
2019-01-30 15:41:09 +0000

    Added dev tools

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
     <nixpkgs/nixos/modules/virtualisation/amazon-image.nix>
     ./dylan.nix
     "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+    ./dev.nix
   ];
 
   ec2.hvm = true;
---
+++ dev.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in
+{
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+  
+  environment.systemPackages = with pkgs; [
+    gcc
+    gnumake
+    unstable.mkl
+  ];
+}
+
