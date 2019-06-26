8c686719b72c49088ffa25acd08f4c2fb5162a34
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2019-06-26 21:15:39 +0000

    Changes for 19.03
    Removed automatic installation of MKL since it takes so long.

--- dev.nix
+++ dev.nix
@@ -1,26 +1,8 @@
 { config, pkgs, ... }:
-
-
-let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
-in
-{
-
-  nixpkgs.config = {
-    allowUnfree = true;
-    packageOverrides = pkgs: {
-      unstable = import unstableTarball {
-        config = config.nixpkgs.config;
-      };
-    };
-  };
-  
+{ 
   environment.systemPackages = with pkgs; [
     gcc
     gnumake
-    unstable.mkl
     awscli
   ];
 }
