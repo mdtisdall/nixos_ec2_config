688d983f07c174484c27c226b84fe06537302a69
System administrator <root@ip-172-31-55-12.ec2.internal>
2020-02-21 03:35:24 +0000

    Added vim config info

--- home-manager.nix
+++ home-manager.nix
@@ -4,7 +4,60 @@
 
   programs.vim = {
     enable = true;
-    plugins = [ "vim-airline" ];
+    plugins = [ "vim-airline" "vim-fugitive" ];
+    extraConfig = ''
+      filetype plugin indent on
+      set nocompatible
+      set modelines=0
+      set nocompatible
+      
+      set modelines=0
+      
+      set encoding=utf-8
+      set scrolloff=3
+      set autoindent
+      set showmode
+      set showcmd
+      set hidden
+      set wildmenu
+      set wildmode=list:longest
+      set visualbell
+      set cursorline
+      set ttyfast
+      set ruler
+      set backspace=indent,eol,start
+      set laststatus=2
+      set relativenumber
+      set undofile
+      
+      set ignorecase
+      set smartcase
+      
+      set wrap
+      set textwidth=0
+      set formatoptions=qrn1
+      set colorcolumn=80
+      set linebreak
+      
+      set makeprg=make
+      
+      set tabstop=2 softtabstop=0 expandtab shiftwidth=2
+      
+      nnoremap <leader><space> :noh<cr>
+      nnoremap <tab> %
+      vnoremap <tab> %
+      
+      noremap <up> <nop>
+      noremap <down> <nop>
+      noremap <left> <nop>
+      noremap <right> <nop>
+      noremap j gj
+      noremap k gk
+      
+      syntax on
+
+      "set statusline+=%{FugitiveStatusline()}"
+    '';
   };
 
   programs.git = {
